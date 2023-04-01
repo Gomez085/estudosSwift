9782737b8762c613ab64f70666b54e86dc462f5d
Bruno Gomes Cavalcante <116772415+Gomez085@users.noreply.github.com>
2023-04-01 16:10:54 +0000

    Closures

--- main.swift
+++ main.swift
@@ -1,29 +1,13 @@
-//   Structs
+//  Closures
 
-struct robotStruct {
-  var name: String
-  let serie: Int
-  let model: String
-
-  func move() {
-    print("moving...")
-  }
-
-  mutating func setNam(newName name: String) {
-     self.name = name
-  }
+func soma(_ n1: Int, _ n2: Int, _ complit: (Int, Int) -> Void){
+  complit(n1, n2)
 }
 
-var robot1 = robotStruct(name: "Alfred", serie: 226, model: "XXZ")
+soma(5, 10) { n1, n2 in
+  print(n1 + n2)          
+}
 
-print("Olá, meu nome é \(robot1.name), minha série é: \(robot1.serie), e meu modelo é: \(robot1.model)")
-print("\nINICIALIZANDO MEU SERVIÇO...")
-robot1.move()
-
-let robot2 = robotStruct(name: "Gordon", serie: 85, model: "BMW")
-
-print("\n\nAqui é assim, meu nome, meu chapa, é: \(robot2.name)")
-print("minha serie é: \(robot2.serie) e meu modelo é O MELHOR de todos, o: \(robot2.model)\n")
-
-robot1.setNam(newName: "Melvin Kelvin")
-print(robot1.name)
+for i in 0...50 {
+  print("\ni =", i)
+}
