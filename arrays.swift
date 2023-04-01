6d428106595469b12b289261b67948ede7ceb0ac
Bruno Gomes Cavalcante <116772415+Gomez085@users.noreply.github.com>
2023-04-01 13:34:43 -0300

    Estudos de arrays

---
+++ arrays.swift
@@ -0,0 +1,14 @@
+// Arrays
+
+var list1: [Int] = [0, 10, 15]
+print(list1)
+list1 += [2, 3, 4]
+print(list1)
+list1.append(5)
+print(list1)
+list1.append(contentsOf: [6, 7, 8])
+print(list1)
+list1.remove(at: 3)
+print(list1)
+list1.insert(100, at: 4)
+print(list1)
